f2a555effacf03205122567a64b898585107c081
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2018-12-20 16:46:14 +0900

    add c99

---
+++ DungeonTemplateLibrary_C99/DungeonRandom.h
@@ -0,0 +1,40 @@
+﻿#ifndef INCLUDED_DUNGEON_TEMPLATE_LIBRARY_DUNGEON_RANDOM
+#define INCLUDED_DUNGEON_TEMPLATE_LIBRARY_DUNGEON_RANDOM
+//:::::----------::::::::::----------::::://
+//     Dungeon Template Library     //
+//          Made by Gaccho.          //
+// This code is licensed under CC0.  //
+//:::::----------::::::::::----------::::://
+
+#include <stdlib.h>
+#include <time.h>
+
+		//初期値
+		void dungeonSeed0() {
+			srand((unsigned int)time(NULL));
+		}
+		void dungeonSeed1(const unsigned int seed_) {
+			srand((unsigned int)seed_);
+		}
+
+		//通常の乱数
+		int dungeonRand0() {
+			return (int)rand();
+		}
+		//0～最大値-1 (余りの範囲の一様分布乱数)
+		int dungeonRand1(const int max_) {
+			return (max_ > 1) ? ((int)(((double)rand() / ((double)RAND_MAX + 1)) * max_)) : 0;
+		}
+		//最小値～最大値
+		int dungeonRand2(const int min_, const int max_) {
+			return (max_ > 1) ? ((int)(((double)rand() / ((double)RAND_MAX + 1)) * (max_ - min_ + 1)) + min_) : min_;
+		}
+		//確率
+		int dungeonRandBool1(const double probability_) {
+			return (((double)rand() / ((double)RAND_MAX + 1)) < probability_) ? 1 : 0;
+		}
+		int dungeonRandBool0() {
+			return ((dungeonRand1(2) == 1) ? 1 : 0);
+		}
+
+#endif //Included Dungeon Template Library
---
+++ DungeonTemplateLibrary_C99/Source.cpp
@@ -0,0 +1,15 @@
+﻿#include "DungeonRandom.h"
+#include <stdio.h>
+
+int main(void) {
+
+	dungeonSeed0();
+
+	for (int i = 0; i < 100; ++i) {
+		//printf("%d\n", dungeonRand1(5));
+		printf("%d\n", dungeonRandBool1(0.8));
+	}
+
+
+	return 0;
+}
