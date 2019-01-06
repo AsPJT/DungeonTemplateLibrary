014077d8bdb6860dd80a9bce965ad729f9bfe97f
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2019-01-07 01:31:01 +0900

    add reversi ai

--- DungeonTemplateLibrary/BoardGame.hpp
+++ DungeonTemplateLibrary/BoardGame.hpp
@@ -8,9 +8,162 @@
 //:::::----------::::::::::----------::::://
 
 #include <cstddef>
+#include <cstdint>
+#include <array>
+#include <limits>
 
 //Dungeon Template Library Namespace
 namespace dtl {
+
+	//指定した場所に駒を置く
+	template<typename Int_, typename STL_>
+	constexpr std::size_t putPieceReversi(STL_& stl_, const std::size_t  x_, const std::size_t y_, const Int_ turn_, const bool is_put_) noexcept {
+		if (stl_.size() == 0) return 0;
+		std::size_t piece_turn_num{};
+		if (stl_[y_][x_] > 0) return 0;
+
+		std::unique_ptr<std::int_fast32_t[]> stl_tmp_x(new std::int_fast32_t[stl_[0].size()]);
+		std::unique_ptr<std::int_fast32_t[]> stl_tmp_y(new std::int_fast32_t[stl_.size()]);
+
+		for (std::int_fast32_t y{ -1 }; y <= 1; ++y)
+			for (std::int_fast32_t x{ -1 }; x <= 1; ++x) {
+				for (std::size_t i{}; i < stl_[0].size(); ++i) stl_tmp_x[i] = 0;
+				for (std::size_t i{}; i < stl_.size(); ++i) stl_tmp_y[i] = 0;
+				for (std::size_t turn_tmp_id{};; ++turn_tmp_id) {
+					std::int_fast32_t turn_x{ static_cast<std::int_fast32_t>(x_) + x * (static_cast<std::int_fast32_t>(turn_tmp_id) + 1) };
+					std::int_fast32_t turn_y{ static_cast<std::int_fast32_t>(y_) + y * (static_cast<std::int_fast32_t>(turn_tmp_id) + 1) };
+					if (turn_x < 0 || turn_x >= stl_[0].size() || turn_y < 0 || turn_y >= stl_.size() || stl_[turn_y][turn_x] == 0) break;
+					if (stl_[turn_y][turn_x] == turn_) {
+						if (is_put_)
+							for (std::size_t i{}; i < turn_tmp_id; ++i)
+								stl_[stl_tmp_y[i]][stl_tmp_x[i]] = turn_;
+						piece_turn_num += turn_tmp_id;
+						break;
+					}
+					stl_tmp_x[turn_tmp_id] = turn_x; stl_tmp_y[turn_tmp_id] = turn_y;
+				}
+			}
+		if (piece_turn_num > 0 && is_put_) stl_[y_][x_] = turn_;
+		return piece_turn_num;
+	}
+	//パスの有無
+	template<typename Int_, typename STL_>
+	constexpr bool isPassReversi(STL_& stl_, const Int_ turn_) noexcept {
+		for (std::size_t i{}; i < stl_.size(); ++i)
+			for (std::size_t j{}; j < stl_[i].size(); ++j)
+				if (putPieceReversi(stl_, j, i, turn_, false)) return false;
+		return true;
+	}
+	//最初に見つけた置ける場所を選ぶ
+	template<typename Int_, typename STL_>
+	constexpr bool reversiAI_Simple(STL_& stl_, const Int_ turn_) noexcept {
+		for (std::size_t i{}; i < stl_.size(); ++i)
+			for (std::size_t j{}; j < stl_[i].size(); ++j)
+				if (putPieceReversi(stl_, j, i, turn_, true)) return true;
+		return true;
+	}
+	//最も多くの駒が取れる場所を選ぶ
+	template<typename Int_, typename STL_>
+	constexpr bool reversiAI_Greed(STL_& stl_, const Int_ turn_) noexcept {
+		std::size_t piece_turn_max{};
+		std::size_t put_piece_x{}, put_piece_y{};
+		for (std::size_t i{}; i < stl_.size(); ++i)
+			for (std::size_t j{}; j < stl_[i].size(); ++j) {
+				const auto& num{ putPieceReversi(stl_, j,i, turn_, false) };
+				if (piece_turn_max < num || (piece_turn_max == num && dtl::rnd.randBool())) {
+					piece_turn_max = num;
+					put_piece_x = j;
+					put_piece_y = i;
+				}
+			}
+		putPieceReversi(stl_, put_piece_x, put_piece_y, turn_, true);
+		return true;
+	}
+	//最も少なく駒が取れる場所を選ぶ
+	template<typename Int_, typename STL_>
+	constexpr bool reversiAI_Unselfishness(STL_& stl_, const Int_ turn_) noexcept {
+		std::size_t piece_turn_min{ std::numeric_limits<std::size_t>::piece_turn_max() };
+		std::size_t put_piece_x{}, put_piece_y{};
+		for (std::size_t i{}; i < stl_.size(); ++i)
+			for (std::size_t j{}; j < stl_[i].size(); ++j) {
+				const auto& num{ putPieceReversi(stl_, j,i, turn_, false) };
+				if (num > 0 && (piece_turn_min > num || (piece_turn_min == num && dtl::rnd.randBool()))) {
+					piece_turn_min = num;
+					put_piece_x = j;
+					put_piece_y = i;
+				}
+			}
+		putPieceReversi(stl_, put_piece_x, put_piece_y, turn_, true);
+		return true;
+	}
+	//優先順位
+	constexpr std::int_fast32_t checkPriority(std::int_fast32_t x_, std::int_fast32_t y_, const std::int_fast32_t x_max_, const std::int_fast32_t y_max_) noexcept {
+		if (x_ == x_max_) x_ = 0;
+		else if (x_ == x_max_ - 1) x_ = 1;
+		else if (x_ == x_max_ - 2) x_ = 2;
+		if (y_ == y_max_) y_ = 0;
+		else if (y_ == y_max_ - 1) y_ = 1;
+		else if (y_ == y_max_ - 2) y_ = 2;
+		switch (x_)
+		{
+		case 0:
+			switch (y_)
+			{
+			case 0:return 0;
+			case 1:return 6;
+			case 2:return 2;
+			default:return 1;
+			}
+		case 1:
+			switch (y_)
+			{
+			case 0:return 6;
+			case 1:return 6;
+			case 2:return 6;
+			default:return 4;
+			}
+		case 2:
+			switch (y_)
+			{
+			case 0:return 2;
+			case 1:return 5;
+			case 2:return 2;
+			default:return 3;
+			}
+		}
+		switch (y_)
+		{
+		case 0:return 1;
+		case 1:return 4;
+		case 2:return 3;
+		default:return 3;
+		}
+		return 0;
+	}
+	//優先順位の高い場所を選ぶ
+	template<typename Int_, typename STL_>
+	constexpr bool reversiAI_Priority(STL_& stl_, const Int_ turn_) noexcept {
+		std::size_t piece_turn_max{};
+		std::size_t put_piece_x{}, put_piece_y{};
+		for (std::uint_fast8_t piece_priority{}; piece_priority < 7 && piece_turn_max == 0; ++piece_priority)
+			for (std::size_t i{}; i < stl_.size(); ++i)
+				for (std::size_t j{}; j < stl_[i].size(); ++j) {
+					if (checkPriority(static_cast<std::int_fast32_t>(j), static_cast<std::int_fast32_t>(i), static_cast<std::int_fast32_t>(stl_[i].size()) - 1, static_cast<std::int_fast32_t>(stl_.size()) - 1) != piece_priority) continue;
+					const auto& num{ putPieceReversi(stl_, j, i, turn_, false) };
+					if (piece_turn_max < num || (piece_turn_max == num && dtl::rnd.randBool())) {
+						piece_turn_max = num;
+						put_piece_x = j;
+						put_piece_y = i;
+					}
+				}
+		putPieceReversi(stl_, put_piece_x, put_piece_y, turn_, true);
+		return true;
+	}
+	//AI同士をまとめる関数
+	template<typename Int_, typename STL_>
+	constexpr bool reversiAI(STL_& stl_, const Int_ turn_, bool(*black_ai_)(STL_&, const Int_), bool(*white_ai_)(STL_&, const Int_), const bool is_black_ai_) noexcept {
+		return (is_black_ai_) ? black_ai_(stl_, turn_) : white_ai_(stl_, turn_);
+	}
 
 	enum :std::size_t {
 		reversi_empty,
