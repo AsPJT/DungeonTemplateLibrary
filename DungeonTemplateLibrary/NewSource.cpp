f1eaca76aa8791d889c2e5e65ffe1f4d229f3727
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2019-01-13 02:57:07 +0900

    add gif

--- DungeonTemplateLibrary/NewSource.cpp
+++ DungeonTemplateLibrary/NewSource.cpp
@@ -1,64 +1,13 @@
 #include "DTL.hpp"
 #include <array>
-#include <bitset>
 
 int main() {
 
-	using matrix_t = std::uint_fast8_t;
+	using dungeon_t = std::uint_fast8_t;
+	std::array<std::array<dungeon_t, 21>, 21> dungeon{ {} };
 
-	//constexpr std::size_t x{ 64 };
-
-	std::array<std::array<matrix_t, 21>, 21> matrix{ {} };
-
-	//dtl::fileRead_CSV<matrix_t>(matrix, "svi.csv");
-
-	////dtl::SimpleVoronoiIsland<matrix_t> svi(matrix, 100, 0.5);
-	//dtl::fileWrite<matrix_t>(matrix, "svi.txt");
-	//dtl::fileWrite_CSV<matrix_t>(matrix, "svi.csv");
-	std::cout << std::endl;
-	dtl::dungeonInit(matrix);
-	dtl::createBorder(matrix);
-	dtl::dungeonStringOutputBool(matrix, "■", "　");
-
-	std::cout << std::endl;
-	dtl::dungeonInit(matrix);
-	dtl::Border<matrix_t> bo(matrix);
-	dtl::dungeonStringOutputBool(matrix, "■", "　");
-
-	std::cout << std::endl;
-	dtl::dungeonInit(matrix);
-	dtl::createBorderOdd(matrix);
-	dtl::dungeonStringOutputBool(matrix, "■", "　");
-
-	std::cout << std::endl;
-	dtl::dungeonInit(matrix);
-	dtl::BorderOdd<matrix_t> boo(matrix);
-	dtl::dungeonStringOutputBool(matrix, "■", "　");
-
-	std::cout << std::endl;
-	dtl::dungeonInit(matrix);
-	dtl::createPointGrid(matrix);
-	dtl::dungeonStringOutputBool(matrix, "■", "　");
-
-	std::cout << std::endl;
-	dtl::dungeonInit(matrix);
-	dtl::PointGrid<matrix_t> pg(matrix);
-	dtl::dungeonStringOutputBool(matrix, "■", "　");
-
-	std::cout << std::endl;
-	dtl::dungeonInit(matrix);
-	dtl::createPointGridField(matrix);
-	dtl::dungeonStringOutputBool(matrix, "■", "　");
-
-	std::cout << std::endl;
-	dtl::dungeonInit(matrix);
-	dtl::PointGridField<matrix_t> pgf(matrix);
-	dtl::dungeonStringOutputBool(matrix, "■", "　");
-
-	std::cout << std::endl;
-	dtl::dungeonInit(matrix);
-	dtl::PointGridFieldPutBlock<matrix_t> pgfpb(matrix);
-	dtl::dungeonStringOutput(matrix, "　", "■", "□");
+	dtl::PointGridField<dungeon_t> point_grid_field(dungeon);
+	dtl::dungeonStringOutputBool(dungeon, "#", " ");
 
 	return 0;
 }
