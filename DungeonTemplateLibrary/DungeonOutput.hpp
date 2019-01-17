b6c5e8b814bac222824a9799cfb70e5fda5e34aa
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2019-01-17 14:41:15 +0900

    add default array output

--- DungeonTemplateLibrary/DungeonOutput.hpp
+++ DungeonTemplateLibrary/DungeonOutput.hpp
@@ -24,6 +24,14 @@ namespace dtl {
 		}
 	}
 	template<typename STL_>
+	constexpr void dungeonNumberOutput(const STL_& stl_, const std::size_t x_, const std::size_t y_) noexcept {
+		for (std::size_t i{}; i < y_; ++i) {
+			for (std::size_t j{}; j < x_; ++j)
+				std::cout << stl_[i][j];
+			std::cout << std::endl;
+		}
+	}
+	template<typename STL_>
 	constexpr void dungeonNumberOutput_RangeBasedFor(const STL_& stl_) noexcept {
 		for (const auto& i : stl_) {
 			for (const auto& j : i)
@@ -36,6 +44,15 @@ namespace dtl {
 		if (string_ == nullptr) return;
 		for (std::size_t i{}; i < stl_.size(); ++i) {
 			for (std::size_t j{}; j < stl_[i].size(); ++j)
+				std::cout << stl_[i][j] << string_;
+			std::cout << std::endl;
+		}
+	}
+	template<typename STL_>
+	constexpr void dungeonNumberOutput(const STL_& stl_, const std::size_t x_, const std::size_t y_, const char* const string_) noexcept {
+		if (string_ == nullptr) return;
+		for (std::size_t i{}; i < y_; ++i) {
+			for (std::size_t j{}; j < x_; ++j)
 				std::cout << stl_[i][j] << string_;
 			std::cout << std::endl;
 		}
@@ -55,6 +72,17 @@ namespace dtl {
 		if (true_ == nullptr || false_ == nullptr) return;
 		for (std::size_t i{}; i < stl_.size(); ++i) {
 			for (std::size_t j{}; j < stl_[i].size(); ++j) {
+				if (stl_[i][j]) std::cout << true_;
+				else std::cout << false_;
+			}
+			std::cout << std::endl;
+		}
+	}
+	template<typename STL_>
+	constexpr void dungeonStringOutputBool(const STL_& stl_, const std::size_t x_, const std::size_t y_, const char* const true_, const char* const false_) noexcept {
+		if (true_ == nullptr || false_ == nullptr) return;
+		for (std::size_t i{}; i < y_; ++i) {
+			for (std::size_t j{}; j < x_; ++j) {
 				if (stl_[i][j]) std::cout << true_;
 				else std::cout << false_;
 			}
--- DungeonTemplateLibrary/NewSource.cpp
+++ DungeonTemplateLibrary/NewSource.cpp
@@ -4,10 +4,19 @@
 int main() {
 
 	using dungeon_t = std::uint_fast8_t;
-	std::array<std::array<dungeon_t, 8>, 8> dungeon{ {} };
+	constexpr std::size_t x_size{ 12 };
+	constexpr std::size_t y_size{ 8 };
 
-	dtl::Chess<dungeon_t> chess(dungeon);
-	dtl::dungeonStringOutput(dungeon, "　", "１", "一", "２", "二", "３", "三", "４", "四", "５", "五", "６", "六");
+	std::array<std::array<dungeon_t, x_size>, y_size> dungeon_stl_array{ {} };
+	dungeon_t dungeon_default_array[y_size][x_size]{};
+
+	dtl::dungeonInit(dungeon_default_array, x_size, y_size);
+
+	dtl::Chess<dungeon_t> chess(dungeon_stl_array);
+	dtl::dungeonStringOutput(dungeon_stl_array, "　", "１", "一", "２", "二", "３", "三", "４", "四", "５", "五", "６", "六");
+
+	dtl::PointGridField<dungeon_t> pgf(dungeon_default_array, x_size, y_size);
+	dtl::dungeonStringOutputBool(dungeon_default_array, x_size, y_size, "■", "　");
 
 	return 0;
 }
