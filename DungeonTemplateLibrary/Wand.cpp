1a49c35868c266c6873aad9a623c03788f564914
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2019-02-12 00:56:38 +0900

    add sample

--- DungeonTemplateLibrary/Wand.cpp
+++ DungeonTemplateLibrary/Wand.cpp
@@ -5,9 +5,14 @@
 int main() {
 	std::array<std::array<std::uint_fast8_t, 150>, 100> stl_{};
 
-	dtl::generator::terrain::stl::SimpleVoronoiIsland<std::uint_fast8_t> svi(stl_, 150, 0.5);
-	dtl::utility::stl::noiseShoreBothBool(stl_, 0.4);
-	dtl::artist::Bucket<std::uint_fast8_t>(stl_, 0, 0, 2);
-	dtl::console::output::stl::string(stl_, "\x1b[46m \x1b[49m", "\x1b[42m \x1b[49m", "\x1b[44m \x1b[49m");
+	using dtl::generator::terrain::stl::SimpleVoronoiIsland;
+	using dtl::utility::stl::noiseShoreBothBool;
+	using dtl::artist::Bucket;
+	using dtl::console::output::stl::string;
+
+	SimpleVoronoiIsland<std::uint_fast8_t> svi(stl_, 150, 0.5);
+	noiseShoreBothBool(stl_, 0.4);
+	Bucket<std::uint_fast8_t>(stl_, 0, 0, 2);
+	string(stl_, "\x1b[46m \x1b[49m", "\x1b[42m \x1b[49m", "\x1b[44m \x1b[49m");
 
 }
--- Sample/mersenne_twister_32bit_1.cpp
+++ Sample/mersenne_twister_32bit_1.cpp
@@ -1,14 +1,15 @@
-﻿#include <iostream>
-#include <cstdint>
-#include <array>
+﻿#include <array>
 #include "DTL.hpp"
 
 int main() {
 
 	using dungeon_t = bool;
 	std::array<std::array<dungeon_t, 10>, 8> matrix{ {} };
+	dtl::generator::common::stl::createBorder(matrix);
 
-	dtl::console::output::stl::number(matrix);
+	dtl::file::write::stl::objBoard<dungeon_t>(matrix, "write_board_test_2.obj", 5, 1);
+
+	dtl::console::output::stl::stringBool(matrix, "##", "  ");
 
 	return 0;
 }
