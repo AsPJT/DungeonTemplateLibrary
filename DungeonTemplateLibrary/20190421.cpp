dbf25f9c8ecf37b308f9579247664c1e819f6a18
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2019-04-23 00:59:02 +0900

    add svi sample class

--- DungeonTemplateLibrary/20190421.cpp
+++ DungeonTemplateLibrary/20190421.cpp
@@ -1,138 +1,34 @@
-﻿//#include <DTL.hpp>
-//#include <cstdint>
-//#include <cstddef>
-//#include <memory>
-//#include <new>
-//
-//int main() {
-//
-//	using shape_t = std::uint_fast8_t;
-//	constexpr std::size_t width{ 256 };
-//	constexpr std::size_t height{ 256 };
-//	constexpr dtl::storage::Image_Palette color_sub{ 32 };
-//
-//	std::unique_ptr<shape_t[][width]> matrix(new(std::nothrow) shape_t[height][width]);
-//
-//	const dtl::utility::Init<shape_t> init(0);
-//	const dtl::shape::FractalLoopIsland<shape_t> dsai(0, 160, 90);
-//
-//	for (std::size_t i{}; i < 1; ++i) {
-//
-//		init.draw(matrix, width, height);
-//		dsai.draw(matrix, width, height);
-//
-//		dtl::storage::FileImage<shape_t, width, height, dtl::storage::primary_colors_rgb>(matrix,
-//			[color_sub](const shape_t value_, dtl::storage::Image_Palette * color) {
-//				color[dtl::storage::image_red] = static_cast<dtl::storage::Image_Palette>(28 + 120 * value_ / 255) / color_sub * color_sub;
-//				color[dtl::storage::image_green] = static_cast<dtl::storage::Image_Palette>(26 + 189 * value_ / 255) / color_sub * color_sub;
-//				color[dtl::storage::image_blue] = static_cast<dtl::storage::Image_Palette>(162 - 83 * value_ / 255) / color_sub * color_sub;
-//			}).writePNG("flisland256_32_" + std::to_string(i) + ".png");
-//
-//	}
-//
-//	return 0;
-//}
-
-#include <DTL.hpp>
+﻿#include <DTL.hpp>
 #include <cstdint>
-#include <cstddef>
-#include <memory>
-#include <new>
-
-class FileImageFunc {
-public:
-	template<typename Matrix_Int_>
-	void operator()(const Matrix_Int_& value_, dtl::storage::Image_Palette* color) noexcept {
-		constexpr dtl::storage::Image_Palette color_sub{ 8 };
-		constexpr Matrix_Int_ deep_max_value{ 100 };
-		constexpr Matrix_Int_ sea_max_value{ 130 };
-		constexpr Matrix_Int_ sand_max_value{ 152 };
-		constexpr Matrix_Int_ land_max_value{ 230 };
-
-		if (value_ <= deep_max_value) {
-			color[dtl::storage::image_red] = static_cast<dtl::storage::Image_Palette>(30);
-			color[dtl::storage::image_green] = static_cast<dtl::storage::Image_Palette>(88);
-			color[dtl::storage::image_blue] = static_cast<dtl::storage::Image_Palette>(126);
-		}
-		else if (value_ <= sea_max_value) {
-			color[dtl::storage::image_red] = static_cast<dtl::storage::Image_Palette>(30 + 135 * (value_ - deep_max_value) / (sea_max_value - deep_max_value)) / color_sub * color_sub;
-			color[dtl::storage::image_green] = static_cast<dtl::storage::Image_Palette>(88 + 133 * (value_ - deep_max_value) / (sea_max_value - deep_max_value)) / color_sub * color_sub;
-			color[dtl::storage::image_blue] = static_cast<dtl::storage::Image_Palette>(126 + 84 * (value_ - deep_max_value) / (sea_max_value - deep_max_value)) / color_sub * color_sub;
-		}
-		else if (value_ <= sand_max_value) {
-			color[dtl::storage::image_red] = static_cast<dtl::storage::Image_Palette>(244 - 20 * (value_ - sea_max_value) / (sand_max_value - sea_max_value)) / color_sub * color_sub;
-			color[dtl::storage::image_green] = static_cast<dtl::storage::Image_Palette>(236 - 27 * (value_ - sea_max_value) / (sand_max_value - sea_max_value)) / color_sub * color_sub;
-			color[dtl::storage::image_blue] = static_cast<dtl::storage::Image_Palette>(215 - 25 * (value_ - sea_max_value) / (sand_max_value - sea_max_value)) / color_sub * color_sub;
-		}
-		else if (value_ <= sand_max_value + 2) {
-			color[dtl::storage::image_red] = static_cast<dtl::storage::Image_Palette>(224 / 2 + 166 / 2);
-			color[dtl::storage::image_green] = static_cast<dtl::storage::Image_Palette>(209 / 2 + 193 / 2);
-			color[dtl::storage::image_blue] = static_cast<dtl::storage::Image_Palette>(190 / 2 + 98 / 2);
-		}
-		else {
-			color[dtl::storage::image_red] = static_cast<dtl::storage::Image_Palette>(166 - 30 * (value_ - sand_max_value) / (land_max_value - sand_max_value)) / color_sub * color_sub;
-			color[dtl::storage::image_green] = static_cast<dtl::storage::Image_Palette>(193 + 12 * (value_ - sand_max_value) / (land_max_value - sand_max_value)) / color_sub * color_sub;
-			color[dtl::storage::image_blue] = static_cast<dtl::storage::Image_Palette>(98 + 1 * (value_ - sand_max_value) / (land_max_value - sand_max_value)) / color_sub * color_sub;
-		}
-
-		color[dtl::storage::image_red] += dtl::random::mt32bit.get<dtl::storage::Image_Palette>(20) - 10;
-		color[dtl::storage::image_green] += dtl::random::mt32bit.get<dtl::storage::Image_Palette>(20) - 10;
-		color[dtl::storage::image_blue] += dtl::random::mt32bit.get<dtl::storage::Image_Palette>(20) - 10;
-
-	}
-};
+#include <array>
 
 int main() {
 
 	using shape_t = std::uint_fast8_t;
 	constexpr std::size_t width{ 256 };
 	constexpr std::size_t height{ 256 };
+	std::array<std::array<shape_t, width>, height> matrix{ {} };
+	//std::array<shape_t, width * height> matrix_array{ {} };
+	//std::array<std::array<std::array<shape_t, 2>, width>, height> matrix_layer{ {} };
 
-	std::unique_ptr<shape_t[][width]> matrix(new(std::nothrow) shape_t[height][width]);
+	//dtl::FractalLoopIsland<shape_t>(dtl::MatrixRange(0,0, 0, 0), 0, 200, 50).draw(matrix);
+	//dtl::thirdParty::stb::FileSTB<shape_t, width, height, 3>().writePNG("fi.png", [](unsigned char* a) {});
+	//dtl::console::OutputStringBool<shape_t>("■", "・").drawOperator(matrix, [](const shape_t value_) {return value_ > 120; });
 
-	const dtl::utility::Init<shape_t> init(0);
-	const dtl::shape::FractalLoopIsland<shape_t> dsai(0, 165, 85);
+	dtl::generator::terrain::stl::SVI<shape_t>().create(matrix, 1500);
+	dtl::console::OutputStringBool<shape_t>("■", "・").draw(matrix);
 
-	for (std::size_t i{}; i < 10; ++i) {
+	//dtl::FractalIsland<shape_t>(dtl::MatrixRange(32, 16, 0, 0), 0, 170, 80).draw(matrix_layer, 0, width, height);
+	//dtl::console::OutputStringBool<shape_t>("■", "・").drawOperator(matrix_layer, 0, width, height, [](const shape_t value_) {return value_ > 50; });
 
-		init.draw(matrix, width, height);
-		dsai.draw(matrix, width, height);
+	//dtl::FractalIsland<shape_t>(dtl::MatrixRange(32, 16, 0, 0), 0, 170, 80).draw(matrix, width, height);
+	//dtl::console::OutputStringBool<shape_t>("■", "・").drawOperator(matrix, width, height, [](const shape_t value_) {return value_ > 50; });
 
-		dtl::storage::FileImage<shape_t, width, height, dtl::storage::primary_colors_rgb>(matrix, FileImageFunc()).writePNG("flislandc256_" + std::to_string(i) + ".png");
+	//dtl::FractalIsland<shape_t>(dtl::MatrixRange(32, 16, 0, 0), 0, 170, 80).draw(matrix_layer, 0);
+	//dtl::console::OutputStringBool<shape_t>("■", "・").drawOperator(matrix_layer, 0, [](const shape_t value_) {return value_ > 50; });
 
-	}
+	//dtl::FractalIsland<shape_t>(dtl::MatrixRange(32, 16, 0, 0), 0, 170, 80).drawArray(matrix_array, width, height);
+	//dtl::console::OutputStringBool<shape_t>("■", "・").drawOperatorArray(matrix_array, width, height, [](const shape_t value_) {return value_ > 50; });
 
 	return 0;
 }
-
-//#include <DTL.hpp>
-//#include <cstdint>
-//#include <array>
-//
-//int main() {
-//
-//	using shape_t = std::uint_fast8_t;
-//	constexpr std::size_t width{ 256 };
-//	constexpr std::size_t height{ 256 };
-//	std::array<std::array<shape_t, width>, height> matrix{ {} };
-//	//std::array<shape_t, width * height> matrix_array{ {} };
-//	//std::array<std::array<std::array<shape_t, 2>, width>, height> matrix_layer{ {} };
-//
-//	dtl::FractalIsland<shape_t>(dtl::MatrixRange(32, 16, 0, 0), 0, 170, 80).draw(matrix);
-//	dtl::thirdParty::stb::FileSTB<shape_t, width, height, 3>().writePNG("fi.png", [](unsigned char* a) {});
-//	//dtl::console::OutputStringBool<shape_t>("■", "・").drawOperator(matrix, [](const shape_t value_) {return value_ > 50; });
-//
-//	//dtl::FractalIsland<shape_t>(dtl::MatrixRange(32, 16, 0, 0), 0, 170, 80).draw(matrix_layer, 0, width, height);
-//	//dtl::console::OutputStringBool<shape_t>("■", "・").drawOperator(matrix_layer, 0, width, height, [](const shape_t value_) {return value_ > 50; });
-//
-//	//dtl::FractalIsland<shape_t>(dtl::MatrixRange(32, 16, 0, 0), 0, 170, 80).draw(matrix, width, height);
-//	//dtl::console::OutputStringBool<shape_t>("■", "・").drawOperator(matrix, width, height, [](const shape_t value_) {return value_ > 50; });
-//
-//	//dtl::FractalIsland<shape_t>(dtl::MatrixRange(32, 16, 0, 0), 0, 170, 80).draw(matrix_layer, 0);
-//	//dtl::console::OutputStringBool<shape_t>("■", "・").drawOperator(matrix_layer, 0, [](const shape_t value_) {return value_ > 50; });
-//
-//	//dtl::FractalIsland<shape_t>(dtl::MatrixRange(32, 16, 0, 0), 0, 170, 80).drawArray(matrix_array, width, height);
-//	//dtl::console::OutputStringBool<shape_t>("■", "・").drawOperatorArray(matrix_array, width, height, [](const shape_t value_) {return value_ > 50; });
-//
-//	return 0;
-//}
--- DungeonTemplateLibrary/Deprecated/SimpleVoronoiIsland.hpp
+++ DungeonTemplateLibrary/Deprecated/SimpleVoronoiIsland.hpp
@@ -137,6 +137,71 @@ namespace dtl::generator::terrain::stl {
 		}
 	};
 
+	template<typename Matrix_Int_>
+	class SVI {
+	public:
+		//コンストラクタ
+		constexpr SVI() noexcept = default;
+
+		//ボロノイ図を作る
+		template<typename Matrix_>
+		void create(Matrix_& matrix_, const std::size_t count_ = 100, const double rbool_ = 0.4, const Matrix_Int_ land_ = 1, const Matrix_Int_ sea_ = 0) const noexcept {
+			//原点の座標と各面の色(もしくは地形データ)を記録する変数
+			std::unique_ptr<Point_Pair_[]> point{ new(std::nothrow) Point_Pair_[count_] };
+			if (!point) return;
+			std::unique_ptr<Matrix_Int_[]> color{ new(std::nothrow) Matrix_Int_[count_] };
+			if (!color) return;
+
+			createPoint(point, color, count_, static_cast<std::int_fast32_t>(dtl::utility::tool::getMatrixSizeX(matrix_)), static_cast<std::int_fast32_t>(dtl::utility::tool::getMatrixSizeY(matrix_)), rbool_, land_, sea_);
+			createSites(point, color, count_, matrix_, dtl::utility::tool::getMatrixSizeX(matrix_), dtl::utility::tool::getMatrixSizeY(matrix_));
+		}
+	private:
+
+		using Point_Pair_ = std::pair<std::int_fast32_t, std::int_fast32_t>;
+
+		//陸地であるか陸地でないか判別する
+		constexpr bool isIsland(const std::unique_ptr<Point_Pair_[]>& point_, const std::size_t point_num_, const std::int_fast32_t w_, const std::int_fast32_t h_, const std::int_fast32_t numerator_, const std::int_fast32_t denominator_) const noexcept {
+			return (point_[point_num_].first > (w_ * numerator_ / denominator_) && point_[point_num_].first < (w_ * (denominator_ - numerator_) / denominator_)) && (point_[point_num_].second > (h_ * numerator_ / denominator_) && point_[point_num_].second < (h_ * (denominator_ - numerator_) / denominator_));
+		}
+		//原点の場所と陸地を決定する
+		constexpr void createPoint(std::unique_ptr<Point_Pair_[]> & point_, std::unique_ptr<Matrix_Int_[]> & color_, const std::size_t count_, const std::int_fast32_t w_, const std::int_fast32_t h_, const double rbool_, const Matrix_Int_ land_, const Matrix_Int_ sea_) const noexcept {
+
+			using dtl::random::mersenne_twister_32bit;
+
+			for (std::size_t i{}, array_num{}; i < count_; ++i) {
+				point_[array_num] = Point_Pair_(mersenne_twister_32bit(w_), mersenne_twister_32bit(h_));
+				if (mersenne_twister_32bit.probability(rbool_))
+					color_[array_num] = land_;
+				else color_[array_num] = sea_;
+				++array_num;
+			}
+		}
+
+		//2点間の距離を返す
+		constexpr std::int_fast32_t distanceSqrd(const Point_Pair_ & point_, std::int_fast32_t x_, std::int_fast32_t y_) const noexcept {
+			x_ -= point_.first;
+			y_ -= point_.second;
+			return x_ * x_ + y_ * y_;
+		}
+
+		//図形を描画
+		template<typename Matrix_>
+		constexpr void createSites(const std::unique_ptr<Point_Pair_[]> & point_, const std::unique_ptr<Matrix_Int_[]> & color_, const std::size_t count_, Matrix_ & matrix_, const std::size_t w_, const std::size_t h_) const noexcept {
+			std::int_fast32_t ds{}, dist{};
+			for (std::size_t hh{}, ind{}; hh < h_; ++hh)
+				for (std::size_t ww{}; ww < w_; ++ww) {
+					ind = (std::numeric_limits<std::size_t>::max)();
+					dist = (std::numeric_limits<std::int_fast32_t>::max)();
+					for (std::size_t it{}; it < count_; ++it) {
+						if ((ds = distanceSqrd(point_[it], static_cast<std::int_fast32_t>(ww), static_cast<std::int_fast32_t>(hh))) >= dist) continue;
+						dist = ds;
+						ind = it;
+					}
+					if (ind != (std::numeric_limits<std::size_t>::max)()) matrix_[hh][ww] = color_[ind];
+				}
+		}
+	};
+
 } //namespace
 
 #endif //Included Dungeon Template Library
