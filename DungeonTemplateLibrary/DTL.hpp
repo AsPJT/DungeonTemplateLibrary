2983e26f903f7582ba7725bf61ab086a0095a777
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2019-04-24 15:49:31 +0900

    add Visual Studio Option

--- DungeonTemplateLibrary/DTL.hpp
+++ DungeonTemplateLibrary/DTL.hpp
@@ -5,6 +5,11 @@
 
 	Distributed under the Boost Software License, Version 1.0. (See accompanying
 	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#######################################################################################
+	Visual Studio Option
+
+	[Language Standard Version] /std:c++latest ( or /std:c++17 or /std:c++14 )
+	[Additional Options] /Zc:__cplusplus
 #######################################################################################*/
 
 #ifndef INCLUDED_DUNGEON_TEMPLATE_LIBRARY
