1e06854c8bd75cdcd5e22b393c54194fdffcb82c
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2019-05-10 10:38:25 +0900

    remove error stb_write

--- include/ThirdParty/STB/stb_image_write.h
+++ include/ThirdParty/STB/stb_image_write.h
@@ -8,82 +8,6 @@
    If using a modern Microsoft Compiler, non-safe versions of CRT calls may cause
    compilation warnings or even errors. To avoid this, also before #including,
 	   #define STBI_MSC_SECURE_CRT
-ABOUT:
-   This header file is a library for writing images to C stdio or a callback.
-   The PNG output is not optimal; it is 20-50% larger than the file
-   written by a decent optimizing implementation; though providing a custom
-   zlib compress function (see STBIW_ZLIB_COMPRESS) can mitigate that.
-   This library is designed for source code compactness and simplicity,
-   not optimal image file size or run-time performance.
-BUILDING:
-   You can #define STBIW_ASSERT(x) before the #include to avoid using assert.h.
-   You can #define STBIW_MALLOC(), STBIW_REALLOC(), and STBIW_FREE() to replace
-   malloc,realloc,free.
-   You can #define STBIW_MEMMOVE() to replace memmove()
-   You can #define STBIW_ZLIB_COMPRESS to use a custom zlib-style compress function
-   for PNG compression (instead of the builtin one), it must have the following signature:
-   unsigned char * my_compress(unsigned char *data, int data_len, int *out_len, int quality);
-   The returned data will be freed with STBIW_FREE() (free() by default),
-   so it must be heap allocated with STBIW_MALLOC() (malloc() by default),
-UNICODE:
-   If compiling for Windows and you wish to use Unicode filenames, compile
-   with
-	   #define STBIW_WINDOWS_UTF8
-   and pass utf8-encoded filenames. Call stbiw_convert_wchar_to_utf8 to convert
-   Windows wchar_t filenames to utf8.
-USAGE:
-   There are five functions, one for each image file format:
-	 int stbi_write_png(char const *filename, int w, int h, int comp, const void *data, int stride_in_bytes);
-	 int stbi_write_bmp(char const *filename, int w, int h, int comp, const void *data);
-	 int stbi_write_tga(char const *filename, int w, int h, int comp, const void *data);
-	 int stbi_write_jpg(char const *filename, int w, int h, int comp, const void *data, int quality);
-	 int stbi_write_hdr(char const *filename, int w, int h, int comp, const float *data);
-	 void stbi_flip_vertically_on_write(int flag); // flag is non-zero to flip data vertically
-   There are also five equivalent functions that use an arbitrary write function. You are
-   expected to open/close your file-equivalent before and after calling these:
-	 int stbi_write_png_to_func(stbi_write_func *func, void *context, int w, int h, int comp, const void  *data, int stride_in_bytes);
-	 int stbi_write_bmp_to_func(stbi_write_func *func, void *context, int w, int h, int comp, const void  *data);
-	 int stbi_write_tga_to_func(stbi_write_func *func, void *context, int w, int h, int comp, const void  *data);
-	 int stbi_write_hdr_to_func(stbi_write_func *func, void *context, int w, int h, int comp, const float *data);
-	 int stbi_write_jpg_to_func(stbi_write_func *func, void *context, int x, int y, int comp, const void *data, int quality);
-   where the callback is:
-	  void stbi_write_func(void *context, void *data, int size);
-   You can configure it with these global variables:
-	  int stbi_write_tga_with_rle;             // defaults to true; set to 0 to disable RLE
-	  int stbi_write_png_compression_level;    // defaults to 8; set to higher for more compression
-	  int stbi_write_force_png_filter;         // defaults to -1; set to 0..5 to force a filter mode
-   You can define STBI_WRITE_NO_STDIO to disable the file variant of these
-   functions, so the library will not use stdio.h at all. However, this will
-   also disable HDR writing, because it requires stdio for formatted output.
-   Each function returns 0 on failure and non-0 on success.
-   The functions create an image file defined by the parameters. The image
-   is a rectangle of pixels stored from left-to-right, top-to-bottom.
-   Each pixel contains 'comp' channels of data stored interleaved with 8-bits
-   per channel, in the following order: 1=Y, 2=YA, 3=RGB, 4=RGBA. (Y is
-   monochrome color.) The rectangle is 'w' pixels wide and 'h' pixels tall.
-   The *data pointer points to the first byte of the top-left-most pixel.
-   For PNG, "stride_in_bytes" is the distance in bytes from the first byte of
-   a row of pixels to the first byte of the next row of pixels.
-   PNG creates output files with the same number of components as the input.
-   The BMP format expands Y to RGB in the file format and does not
-   output alpha.
-   PNG supports writing rectangles of data even when the bytes storing rows of
-   data are not consecutive in memory (e.g. sub-rectangles of a larger image),
-   by supplying the stride between the beginning of adjacent rows. The other
-   formats do not. (Thus you cannot write a native-format BMP through the BMP
-   writer, both because it is in BGR order and because it may have padding
-   at the end of the line.)
-   PNG allows you to set the deflate compression level by setting the global
-   variable 'stbi_write_png_compression_level' (it defaults to 8).
-   HDR expects linear float data. Since the format is always 32-bit rgb(e)
-   data, alpha (if provided) is discarded, and for monochrome data it is
-   replicated across all three channels.
-   TGA supports RLE or non-RLE compressed data. To use non-RLE-compressed
-   data, set the global variable 'stbi_write_tga_with_rle' to 0.
-
-   JPEG does ignore alpha channels in input data; quality is between 1 and 100.
-   Higher quality looks better but results in a bigger image.
-   JPEG baseline (no JPEG progressive).
 CREDITS:
    Sean Barrett           -    PNG/BMP/TGA
    Baldur Karlsson        -    HDR
@@ -1515,74 +1439,9 @@ STBIWDEF int stbi_write_jpg(char const* filename, int x, int y, int comp, const 
 
 #endif // STB_IMAGE_WRITE_IMPLEMENTATION
 
-/* Revision history
-	  1.10  (2019-02-07)
-			 support utf8 filenames in Windows; fix warnings and platform ifdefs
-	  1.09  (2018-02-11)
-			 fix typo in zlib quality API, improve STB_I_W_STATIC in C++
-	  1.08  (2018-01-29)
-			 add stbi__flip_vertically_on_write, external zlib, zlib quality, choose PNG filter
-	  1.07  (2017-07-24)
-			 doc fix
-	  1.06 (2017-07-23)
-			 writing JPEG (using Jon Olick's code)
-	  1.05   ???
-	  1.04 (2017-03-03)
-			 monochrome BMP expansion
-	  1.03   ???
-	  1.02 (2016-04-02)
-			 avoid allocating large structures on the stack
-	  1.01 (2016-01-16)
-			 STBIW_REALLOC_SIZED: support allocators with no realloc support
-			 avoid race-condition in crc initialization
-			 minor compile issues
-	  1.00 (2015-09-14)
-			 installable file IO function
-	  0.99 (2015-09-13)
-			 warning fixes; TGA rle support
-	  0.98 (2015-04-08)
-			 added STBIW_MALLOC, STBIW_ASSERT etc
-	  0.97 (2015-01-18)
-			 fixed HDR asserts, rewrote HDR rle logic
-	  0.96 (2015-01-17)
-			 add HDR output
-			 fix monochrome BMP
-	  0.95 (2014-08-17)
-			   add monochrome TGA output
-	  0.94 (2014-05-31)
-			 rename private functions to avoid conflicts with stb_image.h
-	  0.93 (2014-05-27)
-			 warning fixes
-	  0.92 (2010-08-01)
-			 casts to unsigned char to fix warnings
-	  0.91 (2010-07-17)
-			 first public release
-	  0.90   first internal release
-*/
-
 /*
 ------------------------------------------------------------------------------
-This software is available under 2 licenses -- choose whichever you prefer.
-------------------------------------------------------------------------------
-ALTERNATIVE A - MIT License
-Copyright (c) 2017 Sean Barrett
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-------------------------------------------------------------------------------
-ALTERNATIVE B - Public Domain (www.unlicense.org)
+Public Domain (www.unlicense.org)
 This is free and unencumbered software released into the public domain.
 Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
 software, either in source code form or as a compiled binary, for any purpose,
