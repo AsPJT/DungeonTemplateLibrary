38f5de318160e0dd4250e943f6c3cbdfb51c1ef4
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2019-05-14 01:13:13 +0900

    v0.4.6.1

--- MSVC/DungeonTemplateLibrary/20190502.cpp
+++ MSVC/DungeonTemplateLibrary/20190502.cpp
@@ -1,16 +1,11 @@
 ﻿#include <DTL.hpp>
 #include <array>
-#include <string>
 
 int main() {
 
-	std::array<std::array<int, 10>, 10> matrix{ {} };
+	std::array<std::bitset<10>, 10> matrix{ {} };
 
 	dtl::Border<int>(1).draw(matrix);
-
-	std::string str{};
-	dtl::board::WriteNumber<int>("\n", "(", "), ").draw(matrix, str);
-
-	std::cout << str;
+	dtl::OutputNumber<int>(",").draw(matrix);
 
 }
--- include/Shape/PointGrid.hpp
+++ include/Shape/PointGrid.hpp
@@ -1,24 +1,4 @@
-﻿/*#######################################################################################
-	Copyright (c) 2017-2019 Kasugaccho
-	Copyright (c) 2018-2019 As Project
-	https://github.com/Kasugaccho/DungeonTemplateLibrary
-	wanotaitei@gmail.com
-
-	Distributed under the Boost Software License, Version 1.0. (See accompanying
-	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-#######################################################################################*/
-#ifndef INCLUDED_DUNGEON_TEMPLATE_LIBRARY_SHAPE_POINT_GRID_HPP
-#define INCLUDED_DUNGEON_TEMPLATE_LIBRARY_SHAPE_POINT_GRID_HPP
-
-/*#######################################################################################
-	日本語リファレンス (Reference-JP)
-	https://github.com/Kasugaccho/DungeonTemplateLibrary/wiki/dtl::shape::PointGrid-(形状クラス)/
-#######################################################################################*/
-
-/* Character Code : UTF-8 (BOM) */
-/* Bug Check : already checked */
-/* Android NDK Compile (Clang 5.0) : already checked */
-
+﻿/*
 #include <Base/Struct.hpp>
 #include <Macros/constexpr.hpp>
 #include <Macros/nodiscard.hpp>
@@ -79,6 +59,427 @@ namespace dtl {
 			///// コンストラクタ /////
 
 			using ShapeBase_t::ShapeBase_t;
+		};
+	}
+}
+
+#endif //Included Dungeon Template Library
+
+*/
+
+/*#######################################################################################
+	Copyright (c) 2017-2019 Kasugaccho
+	Copyright (c) 2018-2019 As Project
+	https://github.com/Kasugaccho/DungeonTemplateLibrary
+	wanotaitei@gmail.com
+
+	Distributed under the Boost Software License, Version 1.0. (See accompanying
+	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#######################################################################################*/
+#ifndef INCLUDED_DUNGEON_TEMPLATE_LIBRARY_SHAPE_POINT_GRID_HPP
+#define INCLUDED_DUNGEON_TEMPLATE_LIBRARY_SHAPE_POINT_GRID_HPP
+
+/*#######################################################################################
+	日本語リファレンス (Reference-JP)
+	https://github.com/Kasugaccho/DungeonTemplateLibrary/wiki/dtl::shape::PointGrid-(%E5%BD%A2%E7%8A%B6%E3%82%AF%E3%83%A9%E3%82%B9)/
+#######################################################################################*/
+
+/* Character Code : UTF-8 (BOM) */
+/* Bug Check : already checked */
+/* Android NDK Compile (Clang 5.0) : already checked */
+
+#include <cstddef>
+#include <cstdint>
+#include <utility>
+#include <Base/Struct.hpp>
+#include <Macros/nodiscard.hpp>
+#include <Macros/constexpr.hpp>
+
+namespace dtl {
+	inline namespace shape {
+
+		//偶数マスを指定した数値で埋める
+		template<typename Matrix_Int_>
+		class PointGrid {
+		private:
+
+
+			///// エイリアス /////
+
+			using Index_Size = std::size_t;
+
+
+
+			///// メンバ変数 /////
+
+			Index_Size start_x{};
+			Index_Size start_y{};
+			Index_Size width{};
+			Index_Size height{};
+			Matrix_Int_ draw_value{};
+
+
+			///// 代入処理 /////
+
+			template<typename Matrix_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				inline void substitutionSTL(Matrix_&& matrix_, const Index_Size end_x_, const Index_Size end_y_) const noexcept {
+				matrix_[end_y_][end_x_] = this->draw_value;
+			}
+			template<typename Matrix_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				inline void substitutionArray(Matrix_&& matrix_, const Index_Size end_x_, const Index_Size end_y_, const Index_Size max_x_) const noexcept {
+				matrix_[end_y_ * max_x_ + end_x_] = this->draw_value;
+			}
+			template<typename Matrix_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				inline void substitutionLayer(Matrix_&& matrix_, const Index_Size layer_, const Index_Size end_x_, const Index_Size end_y_) const noexcept {
+				matrix_[end_y_][end_x_][layer_] = this->draw_value;
+			}
+
+			template<typename Matrix_, typename Function_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				inline void substitutionSTL(Matrix_&& matrix_, const Index_Size end_x_, const Index_Size end_y_, Function_&& function_) const noexcept {
+				if (function_(matrix_[end_y_][end_x_])) matrix_[end_y_][end_x_] = this->draw_value;
+			}
+			template<typename Matrix_, typename Function_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				inline void substitutionArray(Matrix_&& matrix_, const Index_Size end_x_, const Index_Size end_y_, const Index_Size max_x_, Function_&& function_) const noexcept {
+				if (function_(matrix_[end_y_ * max_x_ + end_x_])) matrix_[end_y_ * max_x_ + end_x_] = this->draw_value;
+			}
+			template<typename Matrix_, typename Function_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				inline void substitutionLayer(Matrix_ && matrix_, const Index_Size layer_, const Index_Size end_x_, const Index_Size end_y_, Function_ && function_) const noexcept {
+				if (function_(matrix_[end_y_][end_x_][layer_])) matrix_[end_y_][end_x_][layer_] = this->draw_value;
+			}
+
+
+			///// 基本処理 /////
+
+			//STL
+			template<typename Matrix_, typename ...Args_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				bool drawSTL(Matrix_ && matrix_, const Index_Size end_y_, Args_ && ... args_) const noexcept {
+				for (Index_Size row{ this->start_y }; row < end_y_; row += 2)
+					for (Index_Size col{ this->start_x }; col < matrix_[row].size(); col += 2)
+						this->substitutionSTL(matrix_, col, row, args_...);
+				return true;
+			}
+			template<typename Matrix_, typename ...Args_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				bool drawWidthSTL(Matrix_ && matrix_, const Index_Size end_x_, const Index_Size end_y_, Args_ && ... args_) const noexcept {
+				for (Index_Size row{ this->start_y }; row < end_y_; row += 2)
+					for (Index_Size col{ this->start_x }; col < matrix_[row].size() && col < end_x_; col += 2)
+						this->substitutionSTL(matrix_, col, row, args_...);
+				return true;
+			}
+
+			//LayerSTL
+			template<typename Matrix_, typename ...Args_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				bool drawLayerSTL(Matrix_ && matrix_, const Index_Size layer_, const Index_Size end_y_, Args_ && ... args_) const noexcept {
+				for (Index_Size row{ this->start_y }; row < end_y_; row += 2)
+					for (Index_Size col{ this->start_x }; col < matrix_[row].size(); col += 2)
+						this->substitutionLayer(matrix_, layer_, col, row, args_...);
+				return true;
+			}
+			template<typename Matrix_, typename ...Args_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				bool drawLayerWidthSTL(Matrix_ && matrix_, const Index_Size layer_, const Index_Size end_x_, const Index_Size end_y_, Args_ && ... args_) const noexcept {
+				for (Index_Size row{ this->start_y }; row < end_y_; row += 2)
+					for (Index_Size col{ this->start_x }; col < matrix_[row].size() && col < end_x_; col += 2)
+						this->substitutionLayer(matrix_, layer_, col, row, args_...);
+				return true;
+			}
+
+			//Normal
+			template<typename Matrix_, typename ...Args_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				bool drawNormal(Matrix_ && matrix_, const Index_Size end_x_, const Index_Size end_y_, Args_ && ... args_) const noexcept {
+				for (Index_Size row{ this->start_y }; row < end_y_; row += 2)
+					for (Index_Size col{ this->start_x }; col < end_x_; col += 2)
+						this->substitutionSTL(matrix_, col, row, args_...);
+				return true;
+			}
+
+			//LayerNormal
+			template<typename Matrix_, typename ...Args_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				bool drawLayerNormal(Matrix_ && matrix_, const Index_Size layer_, const Index_Size end_x_, const Index_Size end_y_, Args_ && ... args_) const noexcept {
+				for (Index_Size row{ this->start_y }; row < end_y_; row += 2)
+					for (Index_Size col{ this->start_x }; col < end_x_; col += 2)
+						this->substitutionLayer(matrix_, layer_, col, row, args_...);
+				return true;
+			}
+
+			//Array
+			template<typename Matrix_, typename ...Args_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				bool drawArray(Matrix_ && matrix_, const Index_Size end_x_, const Index_Size end_y_, const Index_Size max_x_, Args_ && ... args_) const noexcept {
+				for (Index_Size row{ this->start_y }; row < end_y_; row += 2)
+					for (Index_Size col{ this->start_x }; col < end_x_; col += 2)
+						this->substitutionArray(matrix_, col, row, max_x_, args_...);
+				return true;
+			}
+
+		public:
+
+
+			///// 情報取得 /////
+
+			DTL_VERSIONING_CPP17_NODISCARD
+				constexpr Index_Size getPointX() const noexcept {
+				return this->start_x;
+			}
+			DTL_VERSIONING_CPP17_NODISCARD
+				constexpr Index_Size getPointY() const noexcept {
+				return this->start_y;
+			}
+			DTL_VERSIONING_CPP17_NODISCARD
+				constexpr Index_Size getWidth() const noexcept {
+				return this->width;
+			}
+			DTL_VERSIONING_CPP17_NODISCARD
+				constexpr Index_Size getHeight() const noexcept {
+				return this->height;
+			}
+			DTL_VERSIONING_CPP17_NODISCARD
+				constexpr Matrix_Int_ getValue() const noexcept {
+				return this->draw_value;
+			}
+
+
+			///// 生成呼び出し /////
+
+			//STL
+			template<typename Matrix_>
+			constexpr bool draw(Matrix_ && matrix_) const noexcept {
+				return (this->width == 0) ? this->drawSTL(std::forward<Matrix_>(matrix_), (this->height == 0 || this->start_y + this->height >= matrix_.size()) ? matrix_.size() : this->start_y + this->height) : this->drawWidthSTL(matrix_, this->start_x + this->width, (this->height == 0 || this->start_y + this->height >= matrix_.size()) ? matrix_.size() : this->start_y + this->height);
+			}
+			template<typename Matrix_, typename Function_>
+			constexpr bool drawOperator(Matrix_ && matrix_, Function_ && function_) const noexcept {
+				return (this->width == 0) ? this->drawSTL(std::forward<Matrix_>(matrix_), (this->height == 0 || this->start_y + this->height >= matrix_.size()) ? matrix_.size() : this->start_y + this->height, function_) : this->drawWidthSTL(matrix_, this->start_x + this->width, (this->height == 0 || this->start_y + this->height >= matrix_.size()) ? matrix_.size() : this->start_y + this->height, function_);
+			}
+
+			//LayerSTL
+			template<typename Matrix_>
+			constexpr bool draw(Matrix_ && matrix_, const Index_Size layer_) const noexcept {
+				return (this->width == 0) ? this->drawLayerSTL(std::forward<Matrix_>(matrix_), layer_, (this->height == 0 || this->start_y + this->height >= matrix_.size()) ? matrix_.size() : this->start_y + this->height) : this->drawLayerWidthSTL(matrix_, layer_, this->start_x + this->width, (this->height == 0 || this->start_y + this->height >= matrix_.size()) ? matrix_.size() : this->start_y + this->height);
+			}
+			template<typename Matrix_, typename Function_>
+			constexpr bool drawOperator(Matrix_ && matrix_, const Index_Size layer_, Function_ && function_) const noexcept {
+				return (this->width == 0) ? this->drawLayerSTL(std::forward<Matrix_>(matrix_), layer_, (this->height == 0 || this->start_y + this->height >= matrix_.size()) ? matrix_.size() : this->start_y + this->height, function_) : this->drawLayerWidthSTL(matrix_, layer_, this->start_x + this->width, (this->height == 0 || this->start_y + this->height >= matrix_.size()) ? matrix_.size() : this->start_y + this->height, function_);
+			}
+
+			//Normal
+			template<typename Matrix_>
+			constexpr bool draw(Matrix_ && matrix_, const Index_Size max_x_, const Index_Size max_y_) const noexcept {
+				return this->drawNormal(std::forward<Matrix_>(matrix_), (this->width == 0 || this->start_x + this->width >= max_x_) ? max_x_ : this->start_x + this->width, (this->height == 0 || this->start_y + this->height >= max_y_) ? max_y_ : this->start_y + this->height);
+			}
+			template<typename Matrix_, typename Function_>
+			constexpr bool drawOperator(Matrix_ && matrix_, const Index_Size max_x_, const Index_Size max_y_, Function_ && function_) const noexcept {
+				return this->drawNormal(std::forward<Matrix_>(matrix_), (this->width == 0 || this->start_x + this->width >= max_x_) ? max_x_ : this->start_x + this->width, (this->height == 0 || this->start_y + this->height >= max_y_) ? max_y_ : this->start_y + this->height, function_);
+			}
+
+			//LayerNormal
+			template<typename Matrix_>
+			constexpr bool draw(Matrix_ && matrix_, const Index_Size layer_, const Index_Size max_x_, const Index_Size max_y_) const noexcept {
+				return this->drawLayerNormal(std::forward<Matrix_>(matrix_), layer_, (this->width == 0 || this->start_x + this->width >= max_x_) ? max_x_ : this->start_x + this->width, (this->height == 0 || this->start_y + this->height >= max_y_) ? max_y_ : this->start_y + this->height);
+			}
+			template<typename Matrix_, typename Function_>
+			constexpr bool drawOperator(Matrix_ && matrix_, const Index_Size layer_, const Index_Size max_x_, const Index_Size max_y_, Function_ && function_) const noexcept {
+				return this->drawLayerNormal(std::forward<Matrix_>(matrix_), layer_, (this->width == 0 || this->start_x + this->width >= max_x_) ? max_x_ : this->start_x + this->width, (this->height == 0 || this->start_y + this->height >= max_y_) ? max_y_ : this->start_y + this->height, function_);
+			}
+
+			//Array
+			template<typename Matrix_>
+			constexpr bool drawArray(Matrix_ && matrix_, const Index_Size max_x_, const Index_Size max_y_) const noexcept {
+				return this->drawArray(std::forward<Matrix_>(matrix_), (this->width == 0 || this->start_x + this->width >= max_x_) ? max_x_ : this->start_x + this->width, (this->height == 0 || this->start_y + this->height >= max_y_) ? max_y_ : this->start_y + this->height, max_x_);
+			}
+			template<typename Matrix_, typename Function_>
+			constexpr bool drawOperatorArray(Matrix_ && matrix_, const Index_Size max_x_, const Index_Size max_y_, Function_ && function_) const noexcept {
+				return this->drawArray(std::forward<Matrix_>(matrix_), (this->width == 0 || this->start_x + this->width >= max_x_) ? max_x_ : this->start_x + this->width, (this->height == 0 || this->start_y + this->height >= max_y_) ? max_y_ : this->start_y + this->height, max_x_, function_);
+			}
+
+
+			///// 生成呼び出しファンクタ /////
+
+			template<typename Matrix_, typename ...Args_>
+			constexpr bool operator()(Matrix_ && matrix_, Args_ && ... args_) const noexcept {
+				return this->draw(std::forward<Matrix_>(matrix_), std::forward<Args_>(args_)...);
+			}
+
+
+			///// ダンジョン行列生成 /////
+
+			template<typename Matrix_, typename ...Args_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				Matrix_&& create(Matrix_ && matrix_, Args_ && ... args_) const noexcept {
+				this->draw(matrix_, std::forward<Args_>(args_)...);
+				return std::forward<Matrix_>(matrix_);
+			}
+			template<typename Matrix_, typename ...Args_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				Matrix_&& createArray(Matrix_ && matrix_, Args_ && ... args_) const noexcept {
+				this->drawArray(matrix_, std::forward<Args_>(args_)...);
+				return std::forward<Matrix_>(matrix_);
+			}
+			template<typename Matrix_, typename ...Args_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				Matrix_&& createOperator(Matrix_ && matrix_, Args_ && ... args_) const noexcept {
+				this->drawOperator(matrix_, std::forward<Args_>(args_)...);
+				return std::forward<Matrix_>(matrix_);
+			}
+			template<typename Matrix_, typename ...Args_>
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				Matrix_&& createOperatorArray(Matrix_ && matrix_, Args_ && ... args_) const noexcept {
+				this->drawOperatorArray(matrix_, std::forward<Args_>(args_)...);
+				return std::forward<Matrix_>(matrix_);
+			}
+
+
+			///// 消去 /////
+
+			//始点座標Xを初期値に戻す
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& clearPointX() noexcept {
+				this->start_x = 0;
+				return *this;
+			}
+			//始点座標Yを初期値に戻す
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& clearPointY() noexcept {
+				this->start_y = 0;
+				return *this;
+			}
+			//範囲の大きさ(X軸方向)を初期値に戻す
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& clearWidth() noexcept {
+				this->width = 0;
+				return *this;
+			}
+			//範囲の大きさ(Y軸方向)を初期値に戻す
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& clearHeight() noexcept {
+				this->height = 0;
+				return *this;
+			}
+			//塗り値を初期値に戻す
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& clearValue() noexcept {
+				const Matrix_Int_ new_draw_value{};
+				this->draw_value = new_draw_value;
+				return *this;
+			}
+			//始点座標(X,Y)を初期値に戻す
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& clearPoint() noexcept {
+				this->clearPointX();
+				this->clearPointY();
+				return *this;
+			}
+			//描画範囲を初期値に戻す
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& clearRange() noexcept {
+				this->clearPointX();
+				this->clearPointY();
+				this->clearWidth();
+				this->clearHeight();
+				return *this;
+			}
+			//全ての値を初期値に戻す
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& clear() noexcept {
+				this->clearRange();
+				this->clearValue();
+				return *this;
+			}
+
+
+			///// 代入 /////
+
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& setPointX(const Index_Size end_x_) noexcept {
+				this->start_x = end_x_;
+				return *this;
+			}
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& setPointY(const Index_Size end_y_) noexcept {
+				this->start_y = end_y_;
+				return *this;
+			}
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& setWidth(const Index_Size width_) noexcept {
+				this->width = width_;
+				return *this;
+			}
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& setHeight(const Index_Size height_) noexcept {
+				this->height = height_;
+				return *this;
+			}
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& setValue(const Matrix_Int_ & draw_value_) noexcept {
+				this->draw_value = draw_value_;
+				return *this;
+			}
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& setPoint(const Index_Size point_) noexcept {
+				this->start_x = point_;
+				this->start_y = point_;
+				return *this;
+			}
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& setPoint(const Index_Size end_x_, const Index_Size end_y_) noexcept {
+				this->start_x = end_x_;
+				this->start_y = end_y_;
+				return *this;
+			}
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& setRange(const Index_Size end_x_, const Index_Size end_y_, const Index_Size length_) noexcept {
+				this->start_x = end_x_;
+				this->start_y = end_y_;
+				this->width = length_;
+				this->height = length_;
+				return *this;
+			}
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& setRange(const Index_Size end_x_, const Index_Size end_y_, const Index_Size width_, const Index_Size height_) noexcept {
+				this->start_x = end_x_;
+				this->start_y = end_y_;
+				this->width = width_;
+				this->height = height_;
+				return *this;
+			}
+			DTL_VERSIONING_CPP14_CONSTEXPR
+				PointGrid& setRange(const dtl::base::MatrixRange & matrix_range_) noexcept {
+				this->start_x = matrix_range_.x;
+				this->start_y = matrix_range_.y;
+				this->width = matrix_range_.w;
+				this->height = matrix_range_.h;
+				return *this;
+			}
+
+
+			///// コンストラクタ /////
+
+			constexpr PointGrid() noexcept = default;
+			constexpr explicit PointGrid(const Matrix_Int_ & draw_value_) noexcept
+				:draw_value(draw_value_) {}
+			constexpr explicit PointGrid(const dtl::base::MatrixRange & matrix_range_) noexcept
+				:start_x(matrix_range_.x), start_y(matrix_range_.y),
+				width(matrix_range_.w), height(matrix_range_.h) {}
+			constexpr explicit PointGrid(const dtl::base::MatrixRange & matrix_range_, const Matrix_Int_ & draw_value_) noexcept
+				:start_x(matrix_range_.x), start_y(matrix_range_.y),
+				width(matrix_range_.w), height(matrix_range_.h),
+				draw_value(draw_value_) {}
+			constexpr explicit PointGrid(const Index_Size end_x_, const Index_Size end_y_, const Index_Size width_, const Index_Size height_) noexcept
+				:start_x(end_x_), start_y(end_y_),
+				width(width_), height(height_) {}
+			constexpr explicit PointGrid(const Index_Size end_x_, const Index_Size end_y_, const Index_Size width_, const Index_Size height_, const Matrix_Int_ & draw_value_) noexcept
+				:start_x(end_x_), start_y(end_y_),
+				width(width_), height(height_),
+				draw_value(draw_value_) {}
 		};
 	}
 }
