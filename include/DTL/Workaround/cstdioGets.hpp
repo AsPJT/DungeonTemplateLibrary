974320b2cdad8a2578ad7f753eecf47737f7c87d
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2019-05-20 09:38:14 +0900

    add cstdioGets include guard

--- include/DTL/Workaround/cstdioGets.hpp
+++ include/DTL/Workaround/cstdioGets.hpp
@@ -1,1 +1,15 @@
-﻿extern "C" char *gets(char *s);
+﻿/*#######################################################################################
+	Copyright (c) 2017-2019 Kasugaccho
+	Copyright (c) 2018-2019 As Project
+	https://github.com/Kasugaccho/DungeonTemplateLibrary
+	wanotaitei@gmail.com
+
+	Distributed under the Boost Software License, Version 1.0. (See accompanying
+	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#######################################################################################*/
+#ifndef INCLUDED_DUNGEON_TEMPLATE_LIBRARY_DTL_WORKAROUND_CSTDIO_GETS_HPP
+#define INCLUDED_DUNGEON_TEMPLATE_LIBRARY_DTL_WORKAROUND_CSTDIO_GETS_HPP
+
+extern "C" char *gets(char *s);
+
+#endif //Included Dungeon Template Library
